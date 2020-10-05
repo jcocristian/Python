1f96569858d225ca7b174ed9529a385fbaa0f446
jcocristian <50723575+jcocristian@users.noreply.github.com>
2020-10-05 14:21:53 -0300

    Update RAT.py

--- RAT/RAT.py
+++ RAT/RAT.py
@@ -7,27 +7,27 @@ if op == '2':
     p = input("Senha:\n>> ")
     d = input('Domínio:\n>> ')
     while True:
-        pc = input('Informe o nome do computador.\n(DTI para Sair):\n>> ').strip().upper()
+        pc = input('Informe o nome do computador.\n(SAIR para Sair):\n>> ').strip().upper()
         op2 = input('1 - Abrir "C:\\" da máquina destino.\n2 - Acessar CMD da máquina destino.\n>> ')
         if op2 == '1':
             system(f'start \\\\{pc}\\c$')
             print(f'\nDisco C de {pc} está aberto em seu Windows Explorer.\n')
         elif op2 == '2':
             psexec = (f'psexec.exe \\\\{pc} -u {d}\\{u} -p {p} cmd.exe')
-            if pc == 'DTI':
+            if pc == 'SAIR':
                 break
             else:
                 print('OK!')
 elif op == '1':
     while True:
-        pc = input('Informe o nome do computador.\n(DTI para Sair):\n>> ').strip().upper()
+        pc = input('Informe o nome do computador.\n(SAIR para Sair):\n>> ').strip().upper()
         op2 = input('1 - Abrir "C:\\" da máquina destino.\n2 - Acessar CMD da máquina destino.\n>> ')
         if op2 == '1':
             system(f'start \\\\{pc}\\c$')
             print(f'\nDisco C de {pc} está aberto em seu Windows Explorer.\n')
         elif op2 == '2':
             psexec = (f'psexec.exe \\\\{pc} -s cmd.exe')
-            if pc == 'DTI':
+            if pc == 'SAIR':
                 break
             else:
                 print('OK!')
